7d5ab3549a3fe0e132b02e3439258a3419b988eb
hoangducbinh <hoangbinh02022002@gmail.com>
2025-06-09 22:20:55 +0700

    Điều chỉnh reset css

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -3,14 +3,13 @@ import { RouterModule, Routes } from '@angular/router';
 
 const routes: Routes = [
   //Route mặc định - chuyển hướng khi truy cập '/'
-  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  { path: '', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) },
   //Route fallback - khi route không tồn tại
-  { path: '**', redirectTo: 'landing' },
+  { path: '**', redirectTo: '' },
 
 
   { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule) }, 
   { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
-  { path: 'landing', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) },
 ];
 
 @NgModule({
