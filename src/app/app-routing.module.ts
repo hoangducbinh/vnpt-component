3ef132edddedf08dca878142675606f64c7fac4e
hoangducbinh <hoangbinh02022002@gmail.com>
2025-06-09 21:40:02 +0700

    config project

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,17 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 
-const routes: Routes = [];
+const routes: Routes = [
+  //Route mặc định - chuyển hướng khi truy cập '/'
+  { path: '', redirectTo: 'landing', pathMatch: 'full' },
+  //Route fallback - khi route không tồn tại
+  { path: '**', redirectTo: 'landing' },
+
+
+  { path: 'about', loadChildren: () => import('./modules/about/about.module').then(m => m.AboutModule) }, 
+  { path: 'user', loadChildren: () => import('./modules/user/user.module').then(m => m.UserModule) },
+  { path: 'landing', loadChildren: () => import('./modules/landing/landing.module').then(m => m.LandingModule) },
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
